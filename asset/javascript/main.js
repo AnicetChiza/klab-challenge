d206f1653ae373504ef60795a4f1c6affa7f957a
AnicetChiza <chizaanicet@gmail.com>
2024-08-29 15:13:41 +0200

    add to my repository

--- asset/javascript/main.js
+++ asset/javascript/main.js
@@ -1,4 +1,17 @@
 const menu = document.querySelector('.menu-icon');
 const exit = document.querySelector('.exit-icon');
-const menuExit = document.querySelector('.menu-exit');
+const menuExit = document.querySelector('.exit');
 const divMenu = document.querySelector('.menu');
+const list = document.querySelector('.list ul');
+
+divMenu.addEventListener('click', () => {
+    menu.style.display = 'none';
+    menuExit.style.display = 'flex';
+    list.classList.add('toogle');
+});
+
+exit.addEventListener('click', () => {
+    menu.style.display = 'flex';
+    menuExit.style.display = 'none';
+    list.classList.add('toogle');
+});
