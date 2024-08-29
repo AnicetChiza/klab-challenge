9c9cdb9994b0b9b78607753c9d0a7c4b2ec862d5
AnicetChiza <chizaanicet@gmail.com>
2024-08-29 14:55:46 +0200

    add to my repository

---
+++ asset/javascript/main.js
@@ -0,0 +1,4 @@
+const menu = document.querySelector('.menu-icon');
+const exit = document.querySelector('.exit-icon');
+const menuExit = document.querySelector('.menu-exit');
+const divMenu = document.querySelector('.menu');
